17f56812babe17468e4a897ce23717637cc11964
yash <yashraj.ch2048@gmail.com>
2025-06-09 18:54:39 +0530

    Save state of previous command. New terminal prompt appears after completion of the previous command

--- src/js/cli.js
+++ src/js/cli.js
@@ -1,11 +1,46 @@
-        const input = document.getElementById("cmd")
-        document.addEventListener("keydown", (event) => {
-            const isTypingKey = event.key.length === 1 && !event.ctrlKey && !event.metaKey;
+document.addEventListener("keydown", (event) => {
+    const input = document.getElementById("cmd")
+    const isTypingKey = event.key.length === 1 && !event.ctrlKey && !event.metaKey;
+    if (
+        isTypingKey &&
+        document.activeElement !== input
+    ) {
+        input.focus();
+    }
+     if (event.key === 'Enter') {
+        const inputValue = input.value;
+        const terminalInput = input.closest('.terminal-input');
+        const terminalInputClone = terminalInput.cloneNode(true);
+        terminalInputClone.querySelector('input').value = '';
+        
+        /* Replacing input element with span */
+        const span = document.createElement('span')
+        span.textContent = inputValue;
+        input.parentNode.replaceChild(span, input);
+        
+        /* returns the output to the command */
+        commandHandler(input.value);
 
-            if (
-                isTypingKey &&
-                document.activeElement !== input
-            ) {
-                input.focus();
-            }
-        });
+        /* Appends the terminal input wrapper to the next line */
+        const main = document.querySelector("main")
+        main.append(terminalInputClone)
+
+    }
+});
+
+function commandHandler(cmd) {
+    const args = cmd.trim().split(' ').filter((word) => word.length > 0)
+    switch (args[0]) {
+        case 'exit':
+            window.location.href = '/';
+            break;
+        case 'help':
+
+            break
+
+        default:
+            const ti = document.querySelector('.terminal-input');
+            console.log(document.querySelector('main'))
+            break;
+    }
+}
