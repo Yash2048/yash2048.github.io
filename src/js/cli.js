56fc7389317e4706dc0b1edfc613e2c682686953
yash <yashraj.ch2048@gmail.com>
2025-06-11 04:16:50 +0530

    Added the skills command

--- src/js/cli.js
+++ src/js/cli.js
@@ -1,3 +1,127 @@
+const skills = {
+    "languages":[
+        {
+            "name":"HTML",
+            "className":"devicon-html5-plain"
+        },
+        {
+            "name":"CSS",
+            "className":"devicon-css3-plain"
+        },
+        {
+            "name":"JavaScript",
+            "className":"devicon-javascript-plain"
+        },
+        {
+            "name":"TypeScript",
+            "className":"devicon-typescript-plain"
+        },
+        {
+            "name":"Python",
+            "className":"devicon-python-plain"
+        },
+        {
+            "name":"C++",
+            "className":"devicon-cplusplus-plain"
+        },
+        {
+            "name":"SQL",
+            "className":"devicon-mysql-original"
+        },
+    ],
+    "libraries":[
+        {
+            "name":"React",
+            "className":"devicon-react-original"
+        },
+        {
+            "name":"React Native",
+            "className":"devicon-react-original"
+        },
+        {
+            "name":"React Navigation",
+            "className":"devicon-reactnavigation-original"
+        },
+        {
+            "name":"Tailwind",
+            "className":"devicon-tailwindcss-original"
+        },
+        {
+            "name":"Express",
+            "className":"devicon-express-original"
+        },
+        {
+            "name":"FastAPI",
+            "className":"devicon-fastapi-plain"
+        },
+        {
+            "name":"Vite",
+            "className":"devicon-vitejs-plain"
+        },
+    ],
+    "tools":[
+        {
+            "name":"Git",
+            "className":"devicon-git-plain"
+        },
+        {
+            "name":"GitHub",
+            "className":"devicon-github-original"
+        },
+        {
+            "name":"VS Code",
+            "className":"devicon-vscode-plain"
+        },
+        {
+            "name":"Neovim",
+            "className":"devicon-neovim-plain"
+        },
+        {
+            "name":"npm",
+            "className":"devicon-npm-original-wordmark"
+        },
+        {
+            "name":"pnpm",
+            "className":"devicon-pnpm-plain"
+        },
+        {
+            "name":"GitHub",
+            "className":"devicon-github-original"
+        },
+        {
+            "name":"GitHub",
+            "className":"devicon-github-original"
+        },
+    ],
+    "interested":[
+        {
+            "name":"Rust",
+            "className":"devicon-rust-original"
+        },
+        {
+            "name":"Tauri",
+            "className":"devicon-rust-original"
+        },
+        {
+            "name":"Docker",
+            "className":"devicon-docker-plain"
+        },
+        {
+            "name":"Astro",
+            "className":"devicon-astro-plain"
+        },
+        {
+            "name":"Vue",
+            "className":"devicon-vuejs-plain"
+        },
+        {
+            "name":"Go",
+            "className":"devicon-go-original-wordmark"
+        },
+    ]
+    
+}
+
 document.addEventListener("keydown", (event) => {
     const input = document.getElementById("cmd")
     const isTypingKey = event.key.length === 1 && !event.ctrlKey && !event.metaKey;
@@ -28,7 +152,7 @@ document.addEventListener("keydown", (event) => {
     }
 });
 
-const cmds = ['exit','help','inspiration','whoami']
+const cmds = ['exit', 'help', 'inspiration', 'whoami', 'skills']
 
 function commandHandler(cmd, mainElement, clone) {
     const args = cmd.trim().split(' ').filter((word) => word.length > 0);
@@ -41,10 +165,11 @@ function commandHandler(cmd, mainElement, clone) {
             break;
 
         case 'help':
-            mainElement.innerHTML +=
+            p.innerHTML +=
                 `
-            <p class='output'><span>available commands: </span><span>${cmds.toString().replaceAll(',', ', ')}</span></p>    
+            <span>available commands: </span><span>${cmds.toString().replaceAll(',', ', ')}</span>    
             `
+            mainElement.appendChild(p)
             break;
 
         case 'inspiration':
@@ -62,20 +187,39 @@ function commandHandler(cmd, mainElement, clone) {
             p.textContent = document.querySelector('.usr-name').textContent
             p.className = 'output usr-name'
             mainElement.appendChild(p)
-        break;
+            break;
+
+        case 'skills':
+            const container = document.createElement('div')
+            const skillKeys = Object.keys(skills)
+            const skillsArr = Array(skillKeys.length).fill(null).map(() => document.createElement('div'));
+            skillsArr.forEach((div,index)=>{
+                div.className = skillKeys[index];
+                const nameSpan = document.createElement('span')
+                nameSpan.className = 'skill-name'
+                nameSpan.textContent = `${skillKeys[index]}:`
+                div.appendChild(nameSpan);
+                const skillList = document.createElement('ul')
+                skills[skillKeys[index]].forEach((el)=>{
+                    skillList.innerHTML+=
+                    `<li><i class="${el.className}"></i><p>${el.name}</p></li>`
+                })
+                div.append(skillList)
+                container.append(div)
+            })
+            container.className = 'output skills';
+            mainElement.appendChild(container)
+            break
 
         case undefined:
             break;
 
         default:
-
             const commandSpan = document.createElement("span");
             commandSpan.textContent = `${args[0]}: `;
-
             const errorSpan = document.createElement("span");
             errorSpan.className = "error";
             errorSpan.textContent = "command not found.";
-
             p.appendChild(commandSpan);
             p.appendChild(errorSpan);
             mainElement.appendChild(p);
